870aec66a09298674b57af196787e685b1998dae
Will Fong <willfong@pm.me>
2019-06-04 11:59:18 +0800

    refactored login flow

--- app/app.py
+++ app/app.py
@@ -8,34 +8,23 @@ blueprint = Blueprint("app", __name__, url_prefix="/app")
 
 
 @blueprint.route("/")
-@auth.login_required
 def home():
-    resp = github.get("/user")
-    assert resp.ok
-    oauth_resp = resp.json()
-    # TODO: Should be checking for errors from DB
-    query = "INSERT INTO users (username, name, avatar, last_login) VALUES (%s, %s, %s, NOW()) ON CONFLICT (username) DO UPDATE SET last_login = NOW() RETURNING id"
-    # GitHub/Oauth returns keys with values of None. Can't use .get() defaults, need to use or.
-    params = (
-        oauth_resp["login"],
-        oauth_resp.get("name", "") or "",
-        oauth_resp.get("avatar_url", "") or "",
-    )
-    session["user_id"] = db.write(query, params, returning=True)
-
+    if 'user_id' not in session:
+        return redirect(url_for("auth.login"))
     users = db.read("SELECT COUNT(*) AS total FROM users", one=True)
-
     posts = db.read(
         "SELECT u.name AS name, u.username AS username, u.avatar AS avatar, p.message AS message, p.created AS created FROM posts AS p INNER JOIN users AS u ON p.author_id = u.id ORDER BY p.created DESC LIMIT 20"
     )
 
     return render_template(
-        "app.html", login=oauth_resp["login"], total_users=users["total"], posts=posts
+        "app.html", total_users=users["total"], posts=posts
     )
 
 
 @blueprint.route("/add", methods=["POST"])
 def add_post():
+    if 'user_id' not in session:
+        return redirect(url_for("auth.login"))
     message = request.form.get("message")
     query = "INSERT INTO posts (author_id, message) VALUES (%s, %s)"
     params = (session["user_id"], message)
--- app/auth.py
+++ app/auth.py
@@ -1,23 +1,50 @@
 import os
-from flask import Blueprint, redirect, url_for, flash, session
+from flask import Blueprint, redirect, url_for, flash, session, render_template
 from flask_dance.contrib.github import make_github_blueprint, github
-
+from . import db
 
 blueprint = Blueprint("auth", __name__, url_prefix="/auth")
 
 login_github = make_github_blueprint(
     client_id=os.environ.get("GITHUB_CLIENT_ID"),
     client_secret=os.environ.get("GITHUB_CLIENT_SECRET"),
+    redirect_to="auth.oauth_github"
 )
 
 
+# TODO: Don't know why my decorator doesn't work.
 def login_required(func):
     def wrapper():
-        if not github.authorized:
-            return redirect(url_for("github.login"))
+        print("Starting login_required")
+        if 'user_id' not in session:
+            return redirect(url_for("auth.login"))
+        print("Already logged in...")
         return func()
 
     return wrapper
+
+
+@blueprint.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@blueprint.route("/oauth/github")
+def oauth_github():
+    resp = github.get("/user")
+    assert resp.ok
+    oauth_resp = resp.json()
+    # TODO: Should be checking for errors from DB
+    query = "INSERT INTO users (username, name, avatar, last_login) VALUES (%s, %s, %s, NOW()) ON CONFLICT (username) DO UPDATE SET last_login = NOW() RETURNING id"
+    # GitHub/Oauth returns keys with values of None. Can't use .get() defaults, need to use or.
+    params = (
+        oauth_resp["login"],
+        oauth_resp.get("name", "") or "",
+        oauth_resp.get("avatar_url", "") or "",
+    )
+    session["user_id"] = db.write(query, params, returning=True)
+    flash("Successfully logged in via GitHub!", "success")
+    return redirect(url_for("app.home"))
 
 
 @blueprint.route("/logout")
